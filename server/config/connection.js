e7d8c62f0ddf40a8efea5ef7611b4e363bf792a5
Lexie Hansen <home@Lexies-MacBook-Pro.local>
2021-05-18 13:00:34 -0600

    test seeding for heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://hansenlexie:mongopassword@cluster0.5gst7.mongodb.net/shop_shop?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
